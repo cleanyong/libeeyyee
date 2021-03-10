c5270bf101c2d0cfe93e97d21c19900be34bff33
cleanyong <cleanyong2010@gmail.com>
2021-03-10 13:48:59 -0500

    not smooth with RustCrypto

--- src/lib.rs
+++ src/lib.rs
@@ -32,30 +32,4 @@ mod tests {
 
     }
 
-    #[test]
-    fn it_signs() {
-
-        #[cfg(feature = "ecdsa")]
-         use k256::{
-             ecdsa::{SigningKey, Signature, signature::Signer},
-             SecretKey,
-         };
-         use rand_core::OsRng; // requires 'getrandom' feature
-        
-         // Signing
-         let signing_key = SigningKey::random(&mut OsRng); // Serialize with `::to_bytes()`
-         let message = b"ECDSA proves knowledge of a secret number in the context of a single message";
-        
-         // Note: the signature type must be annotated or otherwise inferrable as
-         // `Signer` has many impls of the `Signer` trait (for both regular and
-         // recoverable signature types).
-         let signature: Signature = signing_key.sign(message);
-        
-         // Verification
-         use k256::{EncodedPoint, ecdsa::{VerifyingKey, signature::Verifier}};
-        
-         let verify_key = VerifyingKey::from(&signing_key); // Serialize with `::to_encoded_point()`
-         println!("good here");
-         assert!(verify_key.verify(message, &signature).is_ok());
-    }
 }
